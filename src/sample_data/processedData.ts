4b0c6d7be23b649e7a2f2e857321bc113b61973a
zes <30737736+zscc@users.noreply.github.com>
2021-08-21 17:56:05 -0700

    new processedData

--- src/sample_data/processedData.ts
+++ src/sample_data/processedData.ts
@@ -2,7 +2,7 @@ import { TransactionTag } from '../constants/transaction-fetcher';
 
 export const processedResponse = [
   {
-    txHash: 0xfb0973dfad0f82845d9aca31b0283cb288113ae8b6781e0445cc451d6449c030,
+    txHash: '0xfb0973dfad0f82845d9aca31b0283cb288113ae8b6781e0445cc451d6449c030',
     labels: [TransactionTag.ERC20_TRANSFER],
     events: ['OrdersMatched'],
     time: '2020/01/01 10:10AM',
